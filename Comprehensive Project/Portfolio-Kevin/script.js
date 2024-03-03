4cf11923a72a521703a9fed4bb4e7323d50eb497
Leeves <leeves.chou@gmail.com>
2024-03-03 22:59:28 +1030

    portifolio v1.0.2

--- Comprehensive Project/Portfolio-Kevin/script.js
+++ Comprehensive Project/Portfolio-Kevin/script.js
@@ -11,3 +11,16 @@ cvNav.addEventListener("click", (e) => {
   cvDes.forEach((ele) => ele.classList.add("hidden"));
   document.getElementById(`des-${e.target.id}`).classList.remove("hidden");
 });
+
+// navigator jump to section
+const mainNav = document.querySelector(".main-nav-list");
+
+const scrollSection = function (e) {
+  e.preventDefault();
+  if (!e.target.classList.contains("main-nav-link")) return;
+  const sectionId = e.target.getAttribute("href");
+  document.querySelector(`.${sectionId.slice(1)}`).scrollIntoView({ behavior: "smooth" });
+  console.log(sectionId);
+};
+mainNav.addEventListener("click", scrollSection);
+
