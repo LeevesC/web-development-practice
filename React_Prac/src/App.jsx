9651ddf4ca8008324f6738cfd7c3d17432c855cd
Leeves <leeves.chou@gmail.com>
2025-02-08 01:06:54 +1030

    react promp

--- React_Prac/src/App.jsx
+++ React_Prac/src/App.jsx
@@ -1,36 +1,79 @@
 import { useState } from "react";
 
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+];
+
 export default function App() {
-  const [advice, setAdvice] = useState("Hello World");
-  const [num, setNum] = useState(0);
-
-  async function getAdvice() {
-    const res = await fetch("https://api.adviceslip.com/advice");
-    const data = await res.json();
-    console.log(data.slip.advice);
-    setAdvice(data.slip.advice);
-    setNum((num) => num + 1);
-  }
-  function reSet() {
-    setNum(0);
-    setAdvice("Hello World");
-  }
-
   return (
     <div>
-      <h1>{advice}</h1>
-      <button onClick={getAdvice}>get advcie</button>
-      <Message a={num} />
-      <button onClick={reSet}>reset</button>
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
 }
 
-function Message(props) {
-  console.log(props);
+function Header() {
+  return <header>Fast React Pizza Co.</header>;
+}
+
+function Menu() {
   return (
-    <p>
-      You have read <strong>{props.a}</strong> pieces of advice
-    </p>
+    <div>
+      <h2>Our menu</h2>
+      {pizzaData.map((pizza) => (
+        <Pizza
+          key={pizza.name}
+          name={pizza.name}
+          ingredients={pizza.ingredients}
+          price={pizza.price}
+        />
+      ))}
+    </div>
+  );
+}
+
+function Pizza(promp) {
+  return (
+    <div>
+      <img src="/" alt="xxx" />
+      <h4>{promp.name}</h4>
+      <p>{promp.ingredients}</p>
+      <span>{promp.price}</span>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer> {new Date().toLocaleTimeString()}We are currently open!</footer>
   );
 }
